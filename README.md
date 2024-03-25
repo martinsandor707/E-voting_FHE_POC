17f17bb222f447568b5c6647e0cae3def0937afa
Sándor Martin Tibor <martinsandor707@gmail.com>
2024-03-25 23:31:25 +0100

    Add README and License

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-voting_FHE_POC
+This is the repository which stores my proof of concept for the use of fully homomorphic encryption during e-voting
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    purge: [ 
+      './src/**/*.{svelte,html,css,js,ts}'
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
+
