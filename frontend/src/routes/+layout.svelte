2fc581d42f7bbdd84b0ebc8017716bb769839f18
Sándor Martin Tibor <martinsandor707@gmail.com>
2024-03-27 00:28:11 +0100

    Experiment with tailwind and shadcn

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
     import '$lib/app.css'
+    import { ModeWatcher } from "mode-watcher";
 </script>
 
 
-<slot></slot>
+<div class="flex flex-col items-center">
+    <ModeWatcher/>
+    <h1 class="text-4xl p-8">E-voting website demo</h1>
+    <div class="h-screen flex justify-center items-center ">
+        <slot class=""></slot>
+    </div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1 class="text-3x1 font-bold underline">Hello world!</h1>
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+    import * as Card from "$lib/components/ui/card";
+</script>
+<Card.Root>
+    <Card.Header>
+      <Card.Title>What do you want to showcase?</Card.Title>
+    </Card.Header>
+    <Card.Content>
+        <p class="w-96">Modern e-voting schemes almost always have at least one thing in common: 
+        the usage of homomorphic encryption! This special kind of encryption lets us
+        do calculations on encrypted data without ever having to decrypt it first.
+        This is extremely useful, since this way we can count the votes without endangering your privacy.
+        </p>
+    </Card.Content>
+    <Card.Footer>
+        <a href="/votes"><img src="voting.jpg" alt="Simplistic art depicting a ballot" width=50%></a>
+    </Card.Footer>
+</Card.Root>
