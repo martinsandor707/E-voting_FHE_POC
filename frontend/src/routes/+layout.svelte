56385cd54ac7fd31b9ac4e7e0d566491bc20e111
Sándor Martin Tibor <martinsandor707@gmail.com>
2024-03-28 00:13:41 +0100

    Update css

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script>
     import '$lib/app.css'
-    import { ModeWatcher } from "mode-watcher";
+    import { ModeWatcher, toggleMode } from "mode-watcher";
+    
 </script>
 
-
+<button class="absolute top-0 right-0 text-4xl" on:click={toggleMode}>🌗</button>
 <div class="flex flex-col items-center">
     <ModeWatcher/>
-    <h1 class="text-4xl p-8">E-voting website demo</h1>
+    <a href="/"><h1 class="text-4xl p-8 text-green-500">E-voting website demo</h1></a>
+
     <div class="h-screen flex justify-center items-center ">
-        <slot class=""></slot>
+        <slot></slot>
     </div>
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,18 +2,35 @@
     import Button from "$lib/components/ui/button/button.svelte";
     import * as Card from "$lib/components/ui/card";
 </script>
-<Card.Root>
+<div class="grid grid-cols-6 gap-8">
+    <div></div>
+    <Card.Root class="col-span-2 hover:border-green-500">
+        <Card.Header>
+            <Card.Title>What do you want to showcase?</Card.Title>
+        </Card.Header>
+        <Card.Content>
+            <p>Modern e-voting schemes almost always have at least one thing in common: 
+            the usage of homomorphic encryption! This special kind of encryption lets us
+            do calculations on encrypted data without ever having to decrypt it first.
+            This is extremely useful, since this way we can count the votes without endangering your privacy.
+            </p>
+        </Card.Content>
+        <Card.Footer class="flex justify-center">
+            <img src="voting.jpg" alt="Simplistic art depicting a ballot" width=60%>
+        </Card.Footer>
+    </Card.Root>
+<Card.Root class="col-span-2 hover:border-green-500">
     <Card.Header>
-      <Card.Title>What do you want to showcase?</Card.Title>
+      <Card.Title>So what now?</Card.Title>
     </Card.Header>
     <Card.Content>
-        <p class="w-96">Modern e-voting schemes almost always have at least one thing in common: 
-        the usage of homomorphic encryption! This special kind of encryption lets us
-        do calculations on encrypted data without ever having to decrypt it first.
-        This is extremely useful, since this way we can count the votes without endangering your privacy.
-        </p>
+        <p>Mainly two things. You can either</p>
     </Card.Content>
-    <Card.Footer>
-        <a href="/votes"><img src="voting.jpg" alt="Simplistic art depicting a ballot" width=50%></a>
+    <Card.Footer class="grid grid-flow gap-y-8 justify-center place-items-center">
+        <a href="/login"><Button>Log in</Button></a>
+        or
+        <a href="/votes"><Button>Check the votes</Button></a>
     </Card.Footer>
 </Card.Root>
+<div></div>
+</div>
